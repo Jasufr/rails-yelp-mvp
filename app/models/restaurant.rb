cd5deba5d40b9391045942c3b3a285ea79a634ee
justin etienne <juetienne7@gmail.com>
2024-02-14 17:05:21 +0900

    images first step done, need to make all the images different and to adapt the size of the profile pictures on notifications

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
-  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  CATEGORIES = ["Chinese", "Italian", "Japanese", "French", "Belgian"]
 
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: CATEGORIES }
