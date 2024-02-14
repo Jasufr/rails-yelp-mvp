19398bed9a697bf5fcb84092e1dc19a4113ce57f
justin etienne <juetienne7@gmail.com>
2024-02-14 16:30:01 +0900

    app done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+# 10.times do
+#   Restaurant.create({ name: Faker::Restaurant.name, category: CATEGORIES.sample, phone_number: Faker::PhoneNumber.phone_number_with_country_code, address: Faker::Address.full_address })
+# end
+
+Restaurant.all.each do |restaurant|
+  5.times do
+    Review.create!({ restaurant_id: restaurant.id, content: Faker::Restaurant.review, rating: (1..5).to_a.sample })
+  end
+end
